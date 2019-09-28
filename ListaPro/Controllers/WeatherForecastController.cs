b03fa00d7f9e21565993f92ad2576372ef6001e0
Ronaldo <ronaldo2385nunes@gmail.com>
2019-09-28 20:19:06 -0300

    two commit

--- ListaPro/Controllers/WeatherForecastController.cs
+++ ListaPro/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace ListaPro.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing142132413", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
